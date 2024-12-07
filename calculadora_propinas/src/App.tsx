548647144c40e4d994949e0c61f9ea1a26cfbdbb
MiltonOscarFlores <miltonoscarflores@gmail.com>
2024-12-07 04:21:00 -0300

    Feat: mostrando el consumo de la orden

--- calculadora_propinas/src/App.tsx
+++ calculadora_propinas/src/App.tsx
@@ -1,9 +1,10 @@
 import { menuItems } from "./data/db";
 import MenuItem from "./components/MenuItem";
+import OrderContents from "./components/OrderContents";
 import useOrder from "./hooks/useOrder";
 
 function App() {
-  const { addItem } = useOrder();
+  const { order, addItem } = useOrder();
 
   return (
     <>
@@ -26,8 +27,8 @@ function App() {
             ))}
           </div>
         </div>
-        <div>
-          <h2>Consumo</h2>
+        <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+          <OrderContents order={order} />
         </div>
       </main>
     </>
